32c2a9c19f38bf1483dbb8641d83c1e1f792f558
HienTran1108 <hientran11082k@gmail.com>
2023-12-17 17:36:11 +0700

    sencond

---
+++ src/components/Button.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+import {createEventDispatcher} from 'svelte';
+export let titleButton: string | undefined;
+const dispatch = createEventDispatcher();
+</script>
+<button 
+    class="w-[200px] h-[52px] p-3 mt-4 border-2 border-orange-500 rounded-md font-bold font-roboto text-orange-500 hover:bg-orange-500 hover:text-white transition-colors duration-300 ease-in-out">
+    {titleButton}
+</button>
